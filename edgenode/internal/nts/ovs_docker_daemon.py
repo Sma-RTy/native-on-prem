22139c3e867479909641a3b3bcc49507f822ba88
Ranya El-Hwigi <66777078+relhwigi@users.noreply.github.com>
2020-09-10 15:38:57 +0100

    [OP-4319] pylint fixes (#21)

--- edgenode/internal/nts/ovs_docker_daemon.py
+++ edgenode/internal/nts/ovs_docker_daemon.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 # coding: utf-8
-
+""" ovs docker daemon """
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (c) 2019-2020 Intel Corporation
 
-import docker
 import argparse
 import logging
 import subprocess
-import os
-import configparser
-import ctypes
 import signal
 import sys
-import time
 import re
+import docker
 
 
 _HOST_NS = "/var/run/docker/netns/default"
@@ -22,11 +18,13 @@ _HOST_NS_MNT = "/var/host_ns/mnt"
 _LOG = None
 
 
-def signal_handler(sig, frame):
+def signal_handler():
+    """ signal handling function """
     _LOG.info("Quiting")
     sys.exit(0)
 
 def make_parser():
+    """ make parser function """
     log_levels = ("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL")
     levels_str = "{0:s} or {1:s}".format(", ".join(log_levels[:-1]), log_levels[-1])
 
@@ -56,6 +54,7 @@ def make_parser():
     return parser
 
 def setup_logger(options):
+    """ setup logger function """
     log_fmt = "OVS DAEMON: [%(levelname)s] %(module)s(%(lineno)d): %(message)s"
     ts_fmt = "%Y-%m-%dT%H:%M:%S"
 
@@ -69,16 +68,17 @@ def setup_logger(options):
 
 
 def docker_connect():
+    """ docker connect function """
     docker_cli = docker.from_env()
     try:
         docker_cli.ping()
-    except docker.errors.APIError as e:
-        _LOG.critical("Failed to connect to docker server\n {}".format(e))
+    except docker.errors.APIError as err:
+        _LOG.critical("Failed to connect to docker server\n {}".format(err))
         return None
     return docker_cli
 
 def create_veth_pair_names(docker_name, name_filter):
-
+    """ create veth pair names function """
     if docker_name.startswith(name_filter):
         offset = len(name_filter)
         return "ve1-"+docker_name[offset:offset+9], "ve2-"+docker_name[offset:offset+9]
@@ -87,14 +87,16 @@ def create_veth_pair_names(docker_name, name_filter):
 
 
 def run_command(command, expected_output):
+    """ run command function """
     try:
         ret = subprocess.check_output(command).decode("utf-8")
-    except subprocess.CalledProcessError as e:
-        _LOG.error("\"{}\" failed[{}]: {}".format(' '.join(e.cmd), e.returncode, e.output))
+    except subprocess.CalledProcessError as err:
+        _LOG.error("\"{}\" failed[{}]: {}".format(' '.join(err.cmd), err.returncode, err.output))
         return False
     return expected_output in ret
 
 def move_if(dst_ip_ns_path, if_name):
+    """ move if function """
     command_prefix = ["ip",
                       "link",
                       "set",
@@ -103,9 +105,9 @@ def move_if(dst_ip_ns_path, if_name):
 
     move_to_host = command_prefix + [_HOST_NS]
 
-    move_to_dst =  ["nsenter",
-                    "--mount=" + _HOST_NS_MNT,
-                    "--net=" + _HOST_NS] + \
+    move_to_dst = ["nsenter",
+                   "--mount=" + _HOST_NS_MNT,
+                   "--net=" + _HOST_NS] + \
                     command_prefix + \
                     [dst_ip_ns_path]
 
@@ -120,18 +122,19 @@ def move_if(dst_ip_ns_path, if_name):
     return True
 
 def move_if_to_host(if_name, bridge_name):
+    """ move if to host function """
     command_prefix = ["ip",
-                        "link",
-                        "set",
-                        if_name,
-                        "netns"]
+                      "link",
+                      "set",
+                      if_name,
+                      "netns"]
 
     move_to_host = command_prefix + [_HOST_NS]
 
-    add_to_ovs =  ["nsenter",
-                    "--mount=" + _HOST_NS_MNT,
-                    "--net=" + _HOST_NS] + \
-    [ "/usr/local/bin/ovs-vsctl", "add-port", bridge_name, if_name]
+    add_to_ovs = ["nsenter",
+                  "--mount=" + _HOST_NS_MNT,
+                  "--net=" + _HOST_NS] + \
+    ["/usr/local/bin/ovs-vsctl", "add-port", bridge_name, if_name]
 
     if not run_command(move_to_host, ""):
         _LOG.error("Failed to move {} to the default namespace".format(if_name))
@@ -144,74 +147,80 @@ def move_if_to_host(if_name, bridge_name):
     return True
 
 def bring_if_up(docker_name, name_filter):
-    ovsIf, dstIf = create_veth_pair_names(docker_name, name_filter)
-    bring_up =   ["nsenter",
-                    "--mount=" + _HOST_NS_MNT,
-                    "--net=" + _HOST_NS] + \
-    [ "ip", "link", "set", ovsIf, "up"]
+    """ bring if up function """
+    ovs_if = create_veth_pair_names(docker_name, name_filter)
+    bring_up = ["nsenter",
+                "--mount=" + _HOST_NS_MNT,
+                "--net=" + _HOST_NS] + \
+    ["ip", "link", "set", ovs_if, "up"]
 
     if not run_command(bring_up, ""):
-        _LOG.error("Failed to bring interface {} up".format(ovsIf))
+        _LOG.error("Failed to bring interface {} up".format(ovs_if))
         return False
 
     return True
 
 def docker_create_if(docker_name, dst_ip_ns_path, bridge_name, name_filter):
-    ovsIf, dstIf = create_veth_pair_names(docker_name, name_filter)
-    createVethPair = [  "ip",
+    """ docker create if function """
+    ovs_if, dst_if = create_veth_pair_names(docker_name, name_filter)
+    create_veth_pair = ["ip",
                         "link",
                         "add",
-                        ovsIf,
+                        ovs_if,
                         "type",
                         "veth",
                         "peer",
                         "name",
-                        dstIf]
+                        dst_if]
 
-    if not run_command(createVethPair, ""):
+    if not run_command(create_veth_pair, ""):
         _LOG.error("Failed to create veth pair with names vethp1/2" + docker_name)
         return False
 
     # move to ovs host
-    if not move_if_to_host(ovsIf, bridge_name):
+    if not move_if_to_host(ovs_if, bridge_name):
         _LOG.error("Failed to move interface to host")
-        run_command(["ip", "link", "delete" ,ovsIf], "")
-        run_command(["ip", "link", "delete" ,dstIf], "")
+        run_command(["ip", "link", "delete", ovs_if], "")
+        run_command(["ip", "link", "delete", dst_if], "")
 
         return False
 
     # move to docker dst
-    if not move_if(dst_ip_ns_path, dstIf):
+    if not move_if(dst_ip_ns_path, dst_if):
         _LOG.error("Failed to move interface to container " + docker_name)
         return False
 
     return True
 
 def docker_delete_if(docker_name, bridge_name, name_filter):
-
-    ovsIf, dstIf = create_veth_pair_names(docker_name, name_filter)
-    remove_from_ovs =  ["nsenter",
-                    "--mount=" + _HOST_NS_MNT,
-                    "--net=" + _HOST_NS] + [ "/usr/local/bin/ovs-vsctl", "del-port", bridge_name, ovsIf]
+    """ docker delete if function """
+    ovs_if = create_veth_pair_names(docker_name, name_filter)
+    remove_from_ovs = ["nsenter",
+                       "--mount=" + _HOST_NS_MNT,
+                       "--net=" + _HOST_NS] + \
+    ["/usr/local/bin/ovs-vsctl", "del-port", bridge_name, ovs_if]
 
     if not run_command(remove_from_ovs, ""):
-        _LOG.error("Failed to remove interface from ovs: " + ovsIf)
+        _LOG.error("Failed to remove interface from ovs: " + ovs_if)
         return False
 
     return True
 
 def filter_name(pod_name, name_filter):
+    """ filter name """
     ret = pod_name.startswith(name_filter)
     if not ret:
         _LOG.debug("{} name doesn't start with {}, not processing.".format(pod_name, name_filter))
     return ret
 
 def check_if_uuid(uuid_string):
-    regex = re.compile('^[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}\Z', re.I)
+    """ check if uuid function """
+    regex = re.compile('^[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}$',
+                       re.I)
     return bool(regex.match(uuid_string))
 
 def docker_poll(docker_cli, name_filter, bridge_name):
-
+    """ docker poll function """
     events = docker_cli.events(decode=True)
 
     for event in events:
@@ -253,15 +262,15 @@ def docker_poll(docker_cli, name_filter, bridge_name):
 
 
 
-        except Exception as e:
-            _LOG.critical("Docker events error {}".format(e))
+        except RuntimeError as err:
+            _LOG.critical("Docker events error {}".format(err))
 
 
 def main(options):
+    """ main """
     docker_cli = docker_connect()
     if not docker_cli:
         _LOG.info("Failed to connect do docker server")
-        return 1
 
     signal.signal(signal.SIGINT, signal_handler)
 
@@ -271,9 +280,9 @@ def main(options):
     docker_poll(docker_cli, options.name_filter, options.bridge_name)
 
 if __name__ == '__main__':
-    options = make_parser().parse_args()
-    _LOG = setup_logger(options)
-    if options.enable.lower() != "true":
+    OPTIONS = make_parser().parse_args()
+    _LOG = setup_logger(OPTIONS)
+    if OPTIONS.enable.lower() != "true":
         _LOG.info("OVS disabled - shutting down")
         sys.exit()
-    sys.exit(main(options))
+    sys.exit(main(OPTIONS))
