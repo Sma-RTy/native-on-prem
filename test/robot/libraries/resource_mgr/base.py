02a5361697a7e78c18da73f636940da534964596
Cezary Marczak <57795129+cezaryxmarczak@users.noreply.github.com>
2020-09-30 16:20:06 +0200

    Remove 'test' directory (#67)
    Co-authored-by: Patryk Diak <47568946+PatrykDiak@users.noreply.github.com>

--- test/robot/libraries/resource_mgr/base.py
+++
@@ -1,6 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-# Copyright (c) 2020 Intel Corporation
-from sqlalchemy.ext.declarative import declarative_base
-
-# Common declarative base
-Base = declarative_base()
